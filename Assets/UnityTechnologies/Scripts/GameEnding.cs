3a5c8a3ca638d1c1d0d872fe74211210df5b32b7
Erifluffball <101990800+Erifluffball@users.noreply.github.com>
2025-04-25 19:26:50 +0100

    Added Update Method

--- Assets/UnityTechnologies/Scripts/GameEnding.cs
+++ Assets/UnityTechnologies/Scripts/GameEnding.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class GameEnding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float fadeDuration = 1f;
+    public GameObject player;
+
+    bool m_IsPlayerAtExit;
+
+    void OnTriggerEnter (Collider other)
     {
-        
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update ()
     {
-        
+        if(m_IsPlayerAtExit)
+        {
+
+        }
     }
 }
